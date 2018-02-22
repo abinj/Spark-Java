13a3eab920a3acce24f0db88b6802df11a433ed0
abinj <abinj@dexlock.com>
2018-02-22 16:55:09 +0530

    inner join, leftOuterJoin, rightOuterJoin examples added

--- sparkbasicexamples/src/main/java/com/abinj/sparkjava/application/RDDPairAction.java
+++ sparkbasicexamples/src/main/java/com/abinj/sparkjava/application/RDDPairAction.java
@@ -4,6 +4,7 @@ import com.abinj.sparkjava.models.AvgCount;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.Optional;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.spark_project.guava.collect.Iterables;
@@ -19,7 +20,8 @@ public class RDDPairAction {
         SparkConf conf = new SparkConf().setAppName("sparkbasicexamples").setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        /*combineByKey(), Combine values with the same key using a different result type.*/
+
+        //combineByKey(), Combine values with the same key using a different result type.//
         List<Tuple2<String, Integer>> input = new ArrayList<>();
         input.add(new Tuple2<>("python", 1));
         input.add(new Tuple2<>("scala", 2));
@@ -41,7 +43,8 @@ public class RDDPairAction {
         }
 
 
-        /*coGroup(), Group data from both RDDs sharing the same key.*/
+
+        //coGroup(), Group data from both RDDs sharing the same key.//
         List<Tuple2<String, Integer>> input2 = new ArrayList<>();
         input2.add(new Tuple2<>("python", 4));
         input2.add(new Tuple2<>("scala", 2));
@@ -50,6 +53,31 @@ public class RDDPairAction {
         for (Tuple2<String, Integer> entry : grouped.collect()) {
             System.out.println(entry._1() + ": " + entry._2());
         }
+
+
+
+        //join(), Perform an inner join between two RDDs.//
+        JavaPairRDD<String, Tuple2<Integer, Integer>> innerJoinData = pair.join(pair2);
+        for (Map.Entry<String, Tuple2<Integer, Integer>> entry : innerJoinData.collectAsMap().entrySet()) {
+            System.out.println(entry.getKey() + ": [" + entry.getValue()._1() + ", " + entry.getValue()._2() + "]");
+        }
+
+
+
+        //rightOuterJoin(), Perform a join between two RDDs where the key must be present in the first RDD.//
+        JavaPairRDD<String, Tuple2<Optional<Integer>, Integer>> rightOuterJoinData = pair.rightOuterJoin(pair2);
+        for (Map.Entry<String, Tuple2<Optional<Integer>, Integer>> entry : rightOuterJoinData.collectAsMap().entrySet()) {
+            System.out.println(entry.getKey() + ": [" + entry.getValue()._1().orNull() + ", " + entry.getValue()._2() + "]");
+        }
+
+
+
+        //leftOuterJoin(), Perform a join between two RDDs where the key must be present in the other RDD.
+        JavaPairRDD<String, Tuple2<Integer, Optional<Integer>>> leftOuterJoin = pair.leftOuterJoin(pair2);
+        for (Map.Entry<String, Tuple2<Integer, Optional<Integer>>> entry : leftOuterJoin.collectAsMap().entrySet()) {
+            System.out.println(entry.getKey() + ": [" + entry.getValue()._1() + ", " + entry.getValue()._2().orNull() + "]");
+        }
+
 
 
 
