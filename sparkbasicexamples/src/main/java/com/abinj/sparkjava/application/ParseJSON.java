8663a3136388460f12db22b57064721599085bd4
abinj <abinmj656@gmail.com>
2018-02-23 19:36:07 +0530

    ParseJson class updated with required comments

--- sparkbasicexamples/src/main/java/com/abinj/sparkjava/application/ParseJSON.java
+++ sparkbasicexamples/src/main/java/com/abinj/sparkjava/application/ParseJSON.java
@@ -20,6 +20,10 @@ public class ParseJSON {
 
     public static void main(String[] args) {
 
+        if (args.length != 3) {
+            System.out.println("arguments needs: [input_json_file] [output_json_file1] [output_json_file2]");
+        }
+
         //Example_1, read json file and parse it into java model then write back json string
         SparkSession sparkSession = SparkSession
                 .builder()
@@ -29,7 +33,7 @@ public class ParseJSON {
         RDD<String> input = sparkSession.sparkContext().textFile(args[0], 2);
         JavaRDD<Person> result = input.toJavaRDD().map(stringInput -> new ObjectMapper().readValue(stringInput, Person.class));
         JavaRDD<String> formatted = result.map(jsonInput -> new ObjectMapper().writeValueAsString(jsonInput));
-//        formatted.saveAsTextFile(args[1]);
+        formatted.saveAsTextFile(args[1]);
 
 
 
@@ -45,7 +49,6 @@ public class ParseJSON {
         peoplesSchema.show();
 
         peoplesSchema.write().format("json").mode("overwrite").save(args[2]);
-
 
 
         sparkSession.stop();
