2d1ce4c86ca2e2829ff9db1e6bd0e8ade4f7bb16
abinj <abinmj656@gmail.com>
2018-03-27 11:55:49 +0530

    Sql aggregate query over parsed csv dataset completed

--- sparkbasicexamples/src/main/java/com/abinj/sparkjava/application/ParseCSV.java
+++ sparkbasicexamples/src/main/java/com/abinj/sparkjava/application/ParseCSV.java
@@ -2,7 +2,9 @@ package com.abinj.sparkjava.application;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.SparkSession;
+import static org.apache.spark.sql.functions.count;
 
 public class ParseCSV {
 
@@ -12,11 +14,17 @@ public class ParseCSV {
                 .master("local")
                 .appName("sparkbasicexamples")
                 .getOrCreate();
+        SQLContext sqlContext = new SQLContext(spark);
 
         Dataset<Row> csvRdd = spark.read()
-                .option("header", true).format("csv")
-                .load("/home/abin/my_space/java_backend/git_works/Spark-Java/sparkbasicexamples/src/main/resources/assets/iris.csv");
+                .option("mode", "DROPMALFORMED")
+                .option("header", true)
+                .csv("/home/abin/my_space/java_backend/git_works/Spark-Java/sparkbasicexamples/src/main/resources/assets/iris.csv");
         csvRdd.printSchema();
         csvRdd.show();
+
+        Dataset<Row> dfResult = csvRdd.groupBy("species").agg(count("species"));
+        dfResult.printSchema();
+        dfResult.show();
     }
 }
